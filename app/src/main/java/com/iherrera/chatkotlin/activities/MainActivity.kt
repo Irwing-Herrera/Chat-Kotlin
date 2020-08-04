98b2104bd3ef9faaf8f792b11205d49e03a6646e
irwing.herrera <irwing.herrera@kairosds.com>
2020-08-04 18:45:06 -0500

    feat(main): agregar fragments, adapter de viewpager y bottom-navigation-view

--- app/src/main/java/com/iherrera/chatkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/iherrera/chatkotlin/activities/MainActivity.kt
@@ -1,17 +1,87 @@
 package com.iherrera.chatkotlin.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.ThemedSpinnerAdapter
 import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
 import com.iherrera.chatkotlin.R
+import com.iherrera.chatkotlin.activities.adapters.PagerAdapter
+import com.iherrera.chatkotlin.activities.fragments.ChatFragment
+import com.iherrera.chatkotlin.activities.fragments.InfoFragment
+import com.iherrera.chatkotlin.activities.fragments.RatesFragment
 import com.iherrera.chatkotlin.activities.others.ToolbarActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ToolbarActivity() {
+
+    /**
+     * Fragment previamete seleccionado
+     */
+    private var prevBottomSelected: MenuItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbarToLoad(toolbar as Toolbar)
+
+        _setUpViewPager(_getPagerAdapter())
+        _setUpBottomNavigationBar()
+    }
+
+    /**
+     * Agrega todos los fragments al adapter
+     *
+     * @return {PagerAdapter}
+     */
+    private fun _getPagerAdapter() : PagerAdapter {
+        val adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragments(InfoFragment())
+        adapter.addFragments(RatesFragment())
+        adapter.addFragments(ChatFragment())
+        return adapter
+    }
+
+    /**
+     * Detectar cual fragment esta seleccionado
+     *
+     * @param {PagerAdapter} adapter
+     */
+    private fun _setUpViewPager(adapter: PagerAdapter) {
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(position: Int) {
+                if (prevBottomSelected == null)
+                    bottomNavigation.menu.getItem(0).isChecked = false
+                else
+                    prevBottomSelected!!.isChecked = false
+
+                bottomNavigation.menu.getItem(position).isChecked = true
+                prevBottomSelected = bottomNavigation.menu.getItem(position)
+            }
+        })
+    }
+
+    /**
+     * Obtiene el item seleccionado de Bottom Navigation Bar
+     */
+    private fun _setUpBottomNavigationBar() {
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_nav_info -> {
+                    viewPager.currentItem = 0; true
+                }
+                R.id.bottom_nav_rates -> {
+                    viewPager.currentItem = 1; true
+                }
+                R.id.bottom_nav_chat -> {
+                    viewPager.currentItem = 2; true
+                }
+                else -> false
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/iherrera/chatkotlin/activities/adapters/PgerAdapter.kt
@@ -0,0 +1,34 @@
+package com.iherrera.chatkotlin.activities.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    /**
+     * Arreglo de fragments
+     */
+    private val fragmentList = ArrayList<Fragment>()
+
+    /**
+     * Obtener fragmento de listado
+     *
+     * @param {Int} position
+     * @return {Fragment}
+     */
+    override fun getItem(position: Int): Fragment = fragmentList[position]
+
+    /**
+     * Obtiene el tamaño del arreglo de el arreglo de los fragments
+     *
+     * @return {Int}
+     */
+    override fun getCount(): Int = fragmentList.size
+
+    /**
+     * Agregar un fragment al arreglo
+     */
+    fun addFragments(fragment: Fragment) = fragmentList.add(fragment)
+
+}
